b760c6a6fc86106a8c8ace3656df12ba69160636
tansangdcmt <tansang414@gmail.com>
2021-10-13 22:39:10 +0700

    tuan 1

---
+++ src/tuan02/lab2bai3.java
@@ -0,0 +1,15 @@
+package tuan02;
+
+import java.util.Scanner;
+
+public class lab2bai3 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Nhập vào số điện:");
+        double soDien = scanner.nextDouble();
+        if(soDien <= 50){
+            System.out.printf("số tiền phải trả là : %.2f",soDien*1000);
+        }else
+            System.out.printf("số tiền phải trả là : %.2f",50 * 1000 + (soDien - 50) * 1200);
+    }
+}
