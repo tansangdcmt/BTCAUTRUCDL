a01384c2777cd93dfe27b9b5f450cf81d0ea10f9
tansangdcmt <tansang414@gmail.com>
2021-10-17 23:16:10 +0700

    bổ sung tuần 1, 2, 3, 4

---
+++ src/com.sangvt2004110053.tuan03/lab3bai1.java
@@ -0,0 +1,25 @@
+package com.sangvt2004110053.tuan03;
+
+import java.util.Scanner;
+
+import javax.lang.model.util.ElementScanner6;
+
+public class lab3bai1 {
+    public static void main(String[] args) {
+        System.out.println("Nhập vào số nguyên n: ");
+        Scanner scanner = new Scanner(System.in);
+        int soN = scanner.nextInt();
+        boolean kiemTra = true;
+        for(int i =2; i < soN; i++){
+            if(soN % i == 0){ 
+            kiemTra = false;
+            break;
+            }
+        }
+        if(kiemTra == true)
+            System.out.printf("%d là số nguyên tố",soN);
+        else
+            System.out.printf("%d không phải là số nguyên tố",soN);
+    }
+
+}
